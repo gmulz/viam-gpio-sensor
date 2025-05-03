61876ff606418ae9529846ce109ecc83f43eda5b
Grant Mulitz <gamulitz@gmail.com>
2025-05-03 12:27:52 -0400

    viam module for gpio sensor reading

---
+++ src/models/gpio_pin_sensor.py
@@ -0,0 +1,106 @@
+from typing import (Any, ClassVar, Dict, Final, List, Mapping, Optional,
+                    Sequence)
+
+from typing_extensions import Self
+from viam.components.board import Board
+from viam.components.board.board import GPIOPin
+from viam.components.sensor import Sensor
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import Geometry, ResourceName
+from viam.resource.base import ResourceBase
+from viam.resource.easy_resource import EasyResource
+from viam.resource.types import Model, ModelFamily
+from viam.utils import SensorReading, ValueTypes
+
+
+class GpioPinSensor(Sensor, EasyResource):
+    # To enable debug-level logging, either run viam-server with the --debug option,
+    # or configure your resource/machine to display debug logs.
+    MODEL: ClassVar[Model] = Model(
+        ModelFamily("grant-dev", "gpio-pin-sensor"), "gpio-pin-sensor"
+    )
+    board: Board
+    pin_name: str
+
+    @classmethod
+    def new(
+        cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ) -> Self:
+        """This method creates a new instance of this Sensor component.
+        The default implementation sets the name from the `config` parameter and then calls `reconfigure`.
+
+        Args:
+            config (ComponentConfig): The configuration for this resource
+            dependencies (Mapping[ResourceName, ResourceBase]): The dependencies (both implicit and explicit)
+
+        Returns:
+            Self: The resource
+        """
+        return super().new(config, dependencies)
+
+    @classmethod
+    def validate_config(cls, config: ComponentConfig) -> Sequence[str]:
+        """This method allows you to validate the configuration object received from the machine,
+        as well as to return any implicit dependencies based on that `config`.
+
+        Args:
+            config (ComponentConfig): The configuration for this resource
+
+        Returns:
+            Sequence[str]: A list of implicit dependencies
+        """
+        config.attributes.fields
+        
+        if "pin" not in config.attributes.fields:
+            raise ValueError("pin_name is required for GpioPinSensor configuration")
+        
+        if "board" not in config.attributes.fields:
+            raise ValueError("board is required for GpioPinSensor configuration")
+
+        board_name = config.attributes.fields["board"].string_value
+
+        return [board_name]
+
+    def reconfigure(
+        self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ):
+        """This method allows you to dynamically update your service when it receives a new `config` object.
+
+        Args:
+            config (ComponentConfig): The new configuration
+            dependencies (Mapping[ResourceName, ResourceBase]): Any dependencies (both implicit and explicit)
+        """
+        attrs = config.attributes.fields
+        self.pin_name = attrs["pin"]
+        board_name = attrs["board"]
+        self.board = dependencies[Board.get_resource_name(board_name)]
+        return super().reconfigure(config, dependencies)
+
+    async def get_readings(
+        self,
+        *,
+        extra: Optional[Mapping[str, Any]] = None,
+        timeout: Optional[float] = None,
+        **kwargs
+    ) -> Mapping[str, SensorReading]:
+        pin = await self.board.gpio_pin_by_name(self.pin_name)
+        value = await pin.get(timeout=timeout, extra=extra)
+        reading = 1 if value else 0
+        return {"signal": SensorReading(name="signal", value=reading)}
+
+    async def do_command(
+        self,
+        command: Mapping[str, ValueTypes],
+        *,
+        timeout: Optional[float] = None,
+        **kwargs
+    ) -> Mapping[str, ValueTypes]:
+        self.logger.error("`do_command` is not implemented")
+        raise NotImplementedError()
+
+    async def get_geometries(
+        self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None
+    ) -> List[Geometry]:
+        self.logger.error("`get_geometries` is not implemented")
+        raise NotImplementedError()
+
